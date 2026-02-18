8ac00bfa50c49eed51ef4f56491cbce32b24a28f
Jeremy Bennett <jeremylukebennett@comcast.net>
2026-02-18 11:40:00 -0600

    fix: stabilize timer and progress flows

--- js/timer.js
+++ js/timer.js
@@ -1,208 +1,189 @@
 "use strict";
-// console.log("printIt before timeerInititalize", printIt);
 
 let $ = require("jquery");
 let firebase = require("firebase/app");
-let fbConfig = require("./fb-config");
 let fbInteractions = require("./interaction");
-var Timer = require('easytimer');
-var userSliderValue = require("./readSliderValue");
+var Timer = require("easytimer");
 let soundAlert = require("./playAudio");
-let timerDiv = document.getElementById("countdownString");
-let graphIt = require("./graphData");
-
-// Main alarm slider settings
-let intervalFlag = false;
+require("./fb-config");
 let durationValues = [5, 10, 15, 20, 25, 30];
 let newDuration = 5;
 let printIt = require("./printToDom");
+let intervalDurationValues = [0, 1, 2, 3, 4, 5];
+let newIntervalDuration = 0;
+let intervalEnabled = false;
+let activeTimer = null;
+let activeIntervalTimer = null;
+let activeSessionDuration = null;
 
-$(document).on("change", "#slider1", ()=>{
+$(document).on("change", "#slider1", () => {
     let newVal = $("#slider1").val();
     $("#slider1").attr("value", newVal);
-// console.log($("li:contains(10)").text());
-//     if($("li:contains(10)").text() === durationValues[newVal].toString()) {
-//         console.log("MATCHED");
-//     }
-    console.log(durationValues[newVal]);
     newDuration = durationValues[newVal];
 });   
 
-// Interval Slider Settings
-
-let intervalDurationValues = [0, 1, 2, 3, 4, 5];
-let newIntervalDuration;
-
-$(document).on("change", "#slider2", ()=>{
+$(document).on("change", "#slider2", () => {
     let newVal = $("#slider2").val();
-    intervalFlag = true;
     $("#slider2").attr("value", newVal);
-    console.log(intervalDurationValues[newVal]);
     newIntervalDuration = intervalDurationValues[newVal];
+    intervalEnabled = newIntervalDuration > 0;
+
+    if (!intervalEnabled) {
+        stopIntervalTimer();
+    } else if (activeTimer && !activeIntervalTimer) {
+        startIntervalTimer();
+    }
 });  
 
-// // Sound Slider Settings
-
-
-
-
-
-
-// Countdown timer 
 function timerInitialize() {
+    stopSession({ renderMainScreen: false });
+    activeSessionDuration = newDuration;
 
-    // console.log('printIt',printIt);
-    console.log("timer function starts");
-
-// Main Timer
     var timer = new Timer();
-        if(intervalFlag) {
-            console.log("SHOULD RUN INTERVAL FUNCTION NOW");
-            runInterval();
+    activeTimer = timer;
+    timer.start({countdown: true, startValues: {minutes: activeSessionDuration}});
+    $("#countdownString .values").html(timer.getTimeValues().toString());
+
+    timer.addEventListener("secondsUpdated", function () {
+        $("#countdownString .values").html(timer.getTimeValues().toString());
+    });
+
+    timer.addEventListener("targetAchieved", function () {
+        if (timer !== activeTimer) {
+            return;
         }
-        timer.start({countdown: true, startValues: {seconds: newDuration}});
-        $('#countdownString .values').html(timer.getTimeValues().toString());
 
-        timer.addEventListener('secondsUpdated', function (e) {
-            $('#countdownString .values').html(timer.getTimeValues().toString());
-        });
+        stopIntervalTimer();
+        activeTimer = null;
+        soundAlert.alertLaunch();
+        logCompletedSession(activeSessionDuration);
+    });
 
-        timer.addEventListener('targetAchieved', function (e) {
-            console.log("times up");
-            intervalFlag = false;
-            soundAlert.alertLaunch();
-            console.log("this is the value of the alarm that just completed: ", newDuration);
+    if (intervalEnabled && newIntervalDuration > 0) {
+        startIntervalTimer();
+    }
+}
 
-            // Add functionality here that checks if user is logged in. If so, add the date and duration of completed session to users progress
+function startIntervalTimer() {
+    if (!intervalEnabled || newIntervalDuration <= 0) {
+        return;
+    }
 
-            firebase.auth().onAuthStateChanged(firebaseUser => {
-                if(firebaseUser) {
-                  console.log("yer users logged in and times up");
-                // Now I need to add the users progress to the proper node.
-                // console.log('newDuration',newDuration);
-                    let todaysDate = new Date();
-                    let progressToLog = {
-                        sessionDate : todaysDate,
-                        sessionDuration : newDuration,
-                        user : firebaseUser.uid
-                    };
-                fbInteractions.sendUserDurationAndDate(progressToLog);
-                
-                
-                console.log("firebaseUser.uid. Is this sending in the current users uid?: ", firebaseUser.uid);
-                fbInteractions.retrieveUserProgress(firebaseUser.uid)
-                .then((result) => {
+    stopIntervalTimer();
+    var intervalTimer = new Timer();
+    activeIntervalTimer = intervalTimer;
+    intervalTimer.start({countdown: true, startValues: {minutes: newIntervalDuration}});
 
-                    console.log("please god let this be the result I want: ", result);
-                    graphIt.consoleUserData(result);
-
-
-                });
-                // console.log();
-                } else {
-                  // User not logged in
-                  console.log("yer users NOT logged in and times up");
-                }
-              });
-        });
-
-        function runInterval() {
-            if(intervalFlag) {
-                var intervalTimer = new Timer();
-                intervalTimer.start({countdown: true, startValues: {seconds: newIntervalDuration}});
-                intervalTimer.addEventListener('targetAchieved', function (e) {
-                    // When Interval countdown ends, do this:
-                    if(intervalFlag) {
-                        console.log("INTERVAL");
-                        soundAlert.intervalAlertLaunch();
-                        runInterval();
-                    }
-                });
-            }
-            document.addEventListener("click", function(e){
-                if(e.target.id === "pause-btn") {
-                    console.log("pause event");
-                    intervalTimer.pause();
-                }
-            });
-
-            document.addEventListener("click", function(e){
-                if(e.target.id === "resume-btn") {
-                    console.log("resume event");
-
-                    intervalTimer.start();
-
-                        runInterval();
-                    // });
-                }
-            });
-
-
-            document.addEventListener("click", function(e){
-                if(e.target.id === "menuProgress") {
-                    console.log("clicked menu progress");
-
-                    intervalTimer.pause();
-
-                }
-            });
+    intervalTimer.addEventListener("targetAchieved", function () {
+        if (intervalTimer !== activeIntervalTimer || !activeTimer || !intervalEnabled) {
+            return;
         }
-        
-        
-        
-        // This is a Pause function. Still need a back to home function.
-        document.addEventListener("click", function(e){
-            if(e.target.id === "pause-btn") {
-                printIt.printResumeButtonToPage();
 
-                timer.pause();
-                
-                document.getElementById("myAudioBell").pause();
-                document.getElementById("myIntervalAudioBell").pause();
-                document.getElementById("myAudioBlock").pause();
-                document.getElementById("myIntervalAudioBlock").pause();
-                document.getElementById("myAudioTone").pause();
-                document.getElementById("myIntervalAudioTone").pause();
-                intervalFlag = false;
-            }
-        });
-
-        document.addEventListener("click", function(e){
-            if(e.target.id === "resume-btn") {
-                printIt.reprintTimerButtons();
-                timer.start();
-            }
-        });
-
-        document.addEventListener("click", function(e){
-            
-            if(e.target.id === "stop-btn") {
-                timer.stop();
-                document.getElementById("myAudioBell").pause();
-                document.getElementById("myAudioBell").currentTime = 0;
-                document.getElementById("myAudioBlock").pause();
-                document.getElementById("myAudioBlock").currentTime = 0;
-                document.getElementById("myAudioTone").pause();
-                document.getElementById("myAudioTone").currentTime = 0;
-                document.getElementById("myIntervalAudioBell").pause();
-                document.getElementById("myIntervalAudioBell").currentTime = 0;
-                document.getElementById("myIntervalAudioBlock").pause();
-                document.getElementById("myIntervalAudioBlock").currentTime = 0;
-                document.getElementById("myIntervalAudioTone").pause();
-                document.getElementById("myIntervalAudioTone").currentTime = 0;
-
-                intervalFlag = false;
-                console.log("you clicked stop");
-                $("#timer-buttons").hide();
-                printIt.printMainScreen();
-            }
-
-            document.addEventListener("click", function(e){
-                if(e.target.id === "menuProgress") {
-                    timer.pause();
-                }
-        });
+        soundAlert.intervalAlertLaunch();
+        intervalTimer.start({countdown: true, startValues: {minutes: newIntervalDuration}});
     });
 }
 
-    module.exports = {timerInitialize, newDuration};
+function stopIntervalTimer() {
+    if (!activeIntervalTimer) {
+        return;
+    }
+
+    activeIntervalTimer.stop();
+    activeIntervalTimer = null;
+}
+
+function pauseActiveTimers() {
+    if (activeTimer) {
+        activeTimer.pause();
+    }
+    if (activeIntervalTimer) {
+        activeIntervalTimer.pause();
+    }
+    stopAllAudio();
+}
+
+function resumeActiveTimers() {
+    if (activeTimer) {
+        activeTimer.start();
+    }
+    if (activeIntervalTimer && intervalEnabled) {
+        activeIntervalTimer.start();
+    }
+}
+
+function stopAllAudio() {
+    let audioIds = [
+        "myAudioBell",
+        "myIntervalAudioBell",
+        "myAudioBlock",
+        "myIntervalAudioBlock",
+        "myAudioTone",
+        "myIntervalAudioTone"
+    ];
+
+    audioIds.forEach((audioId) => {
+        let audioEl = document.getElementById(audioId);
+        if (audioEl) {
+            audioEl.pause();
+            audioEl.currentTime = 0;
+        }
+    });
+}
+
+function stopSession(options) {
+    let settings = options || {};
+
+    if (activeTimer) {
+        activeTimer.stop();
+        activeTimer = null;
+    }
+    stopIntervalTimer();
+    stopAllAudio();
+
+    if (settings.renderMainScreen) {
+        printIt.printMainScreen();
+    }
+}
+
+function logCompletedSession(sessionDuration) {
+    let firebaseUser = firebase.auth().currentUser;
+    if (!firebaseUser) {
+        return;
+    }
+
+    let progressToLog = {
+        sessionDate: new Date(),
+        sessionDuration: sessionDuration,
+        user: firebaseUser.uid
+    };
+
+    fbInteractions.sendUserDurationAndDate(progressToLog);
+}
+
+$(document).on("click", "#pause-btn", function () {
+    if (!activeTimer) {
+        return;
+    }
+    printIt.printResumeButtonToPage();
+    pauseActiveTimers();
+});
+
+$(document).on("click", "#resume-btn", function () {
+    if (!activeTimer) {
+        return;
+    }
+    printIt.reprintTimerButtons();
+    resumeActiveTimers();
+});
+
+$(document).on("click", "#stop-btn", function () {
+    stopSession({ renderMainScreen: true });
+    $("#timer-buttons").hide();
+});
+
+$(document).on("click", "#menuProgress", function () {
+    pauseActiveTimers();
+});
+
+module.exports = {timerInitialize, newDuration};
